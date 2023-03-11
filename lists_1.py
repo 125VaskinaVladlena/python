e4cc0e7990eeea71d71c57ce71fbc7cf9745a73a
vladlenavas <113258575+vladlenavas@users.noreply.github.com>
2023-03-11 08:37:46 +0300

    Update lists_1.py

--- lists_1.py
+++ lists_1.py
@@ -1,5 +1,5 @@
 import random
 list_1 = random.sample(range(10000), random.randint(50, 100))
 list_2 = random.sample(range(10000), random.randint(50, 100))
-new_list = [num for num in list_1 if num not in list_2] #новый список в которм числа из первого списка которых нет во втором
+new_list = [n for n in list_1 if n not in list_2] #новый список в которм числа (n) из первого списка которых нет во втором
 print(new_list)

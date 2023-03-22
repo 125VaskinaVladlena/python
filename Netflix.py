504350514f02d245ee5b94393d379af88e5295dc
vladlenavas <113258575+vladlenavas@users.noreply.github.com>
2023-03-22 23:36:31 +0300

    Update Netflix.py
    :)

--- Netflix.py
+++ Netflix.py
@@ -1,16 +1,21 @@
 def netflix(x, y, z, cost):
+    if x + y + z <= cost:
+        return "impossible"
+    
     pos_comb = [(x+y, z), (x+z, y), (y+z, x)]
-    best_comb = min(pos_comb, key=lambda comb: abs(comb[0]+comb[1]-cost))
-    if best_comb[0] in [x, y]:
+    best_comb = min(pos_comb, key=lambda comb: abs(comb[0] + comb[1] - cost))
+    
+    if best_comb[0] == x:
         return "Alice", "Bob"
-    elif best_comb[0] in [x, z]:
-        return "Alice", "Charlie"
-    else:
+    elif best_comb[0] == y:
         return "Bob", "Charlie"
+    else:
+        return "Alice", "Charlie"
 
 x = int(input("Alice's money: "))
 y = int(input("Bob's money: "))
 z = int(input("Charlie's money: "))
-cost = int(input("Netflix subscription price: "))
-buyers = netflix(x, y, z, cost)
-print("{} and {} should buy the Netflix subscription".format(buyers[0], buyers[1]))
+cost = int(input("Price: "))
+
+n = netflix(x, y, z, cost)
+print(n)

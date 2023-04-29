0b82f7b7de48d4042a9fa4ae72c0e1735adb6c0c
vladlenavas <113258575+vladlenavas@users.noreply.github.com>
2023-04-29 17:42:41 +0300

    -_-

---
+++ Middle/12_functions.py
@@ -0,0 +1,8 @@
+def sum_range(start, end):
+    if start > end:
+        start, end = end, start
+    return sum(range(start, end+1))
+start=int(input("Start: ")
+End=int(input("End: ")
+sum_range(start,end)
+print
